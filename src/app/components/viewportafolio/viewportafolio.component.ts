4b839c312e1d1081b8bbaec5cad24f32c07aba5b
Arturo González <arturogarza.gonz@hotmail.com>
2024-06-25 14:28:10 -0600

    agregadas nuevas rutas de imagenes

--- src/app/components/viewportafolio/viewportafolio.component.ts
+++ src/app/components/viewportafolio/viewportafolio.component.ts
@@ -16,6 +16,9 @@ export class ViewportafolioComponent implements OnInit {
   images: string[] = [
     'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/prograrecortado.jpg',
     'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/reactrecortado.png',
+    'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/nodeprod.png',
+    'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/angularprod.png',
+    'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/rnprod.png',
   ];
   currentImageIndex: number = 0;
 
