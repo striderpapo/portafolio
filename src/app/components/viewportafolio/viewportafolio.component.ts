73a66d58a1c54af0f6463d6530191696f76c9778
Arturo González <arturogarza.gonz@hotmail.com>
2024-06-25 14:31:40 -0600

    cambido .png a .jpg

--- src/app/components/viewportafolio/viewportafolio.component.ts
+++ src/app/components/viewportafolio/viewportafolio.component.ts
@@ -18,7 +18,7 @@ export class ViewportafolioComponent implements OnInit {
     'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/reactrecortado.png',
     'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/nodeprod.png',
     'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/angularprod.png',
-    'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/rnprod.png',
+    'https://main--portafolioarturogg.netlify.app/portafolio/assets/img/rnprod.jpg',
   ];
   currentImageIndex: number = 0;
 
