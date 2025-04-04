57598a7431a8c0264f9f3e54e02735e0ad3fbc8d
Arturo González <arturogarza.gonz@hotmail.com>
2025-04-03 19:20:27 -0600

    ajustarimagen movil 1

--- src/app/components/viewportafolio/viewportafolio.component.ts
+++ src/app/components/viewportafolio/viewportafolio.component.ts
@@ -15,8 +15,8 @@ export class ViewportafolioComponent implements OnInit {
   erasingSpeed: number = 50; // Velocidad de borrado en milisegundos
   delayBetweenTexts: number = 500; // Retraso entre textos en milisegundos
   images: string[] = [
-    'https://main--portafolioarturogg.netlify.app/assets/img/prograrecortado.jpg',
     'https://main--portafolioarturogg.netlify.app/assets/img/reactrecortado.png',
+    'https://main--portafolioarturogg.netlify.app/assets/img/prograrecortado.jpg',
     'https://main--portafolioarturogg.netlify.app/assets/img/nodeprod.png',
     'https://main--portafolioarturogg.netlify.app/assets/img/angularprod.png',
     'https://main--portafolioarturogg.netlify.app/assets/img/rnprod.png',
